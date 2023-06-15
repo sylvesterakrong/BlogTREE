28e8989ebe46e1d4f448918482ce39b604aa456b
sylvesterakrong <akrongsylvester@gmail.com>
2023-06-15 16:16:26 +0000

    updated navbar(mobile nav)

--- blogtree/src/app/components/Navbar.jsx
+++ blogtree/src/app/components/Navbar.jsx
@@ -1,7 +1,10 @@
 "use client"
 import Link from "next/link";
-
+import { useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUserAstronaut, faMap } from '@fortawesome/free-solid-svg-icons'
 const Navbar = () => {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
    
 
     return (
@@ -15,22 +18,35 @@ const Navbar = () => {
                   </h1>
                 </div>
 
-                  <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="menu" >
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                  </a>
-
+                  
+                    {/*mobile navbar */} 
+                <a role="button" className=" navbar-burger my-4" onClick= {() => setToggleDropdown((prev) => !prev)}>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>        
+              </div>
+              <div>
+                {toggleDropdown && (
+                  <div className="dropdown-content card-content mr-5 ml-5 has-shadow">
+                    <div className="pb-1">
+                      <Link href="/" className="dropdown-item is-size-5" onClick={()=> setToggleDropdown(false)}><FontAwesomeIcon icon={faHome} /> home</Link>
+                      <Link href="/pages/About" className="dropdown-item is-size-5" onClick={()=> setToggleDropdown(false)}><FontAwesomeIcon icon={faMap}/>  about</Link>                         
+                      <Link href="/pages/Profile" className="dropdown-item is-size-5" onClick={()=> setToggleDropdown(false)}><FontAwesomeIcon icon={faUserAstronaut}/> profile
+                      </Link>
+                    </div>
+                  </div>
+                )}
               </div>
 
-              <div id="menu" className="navbar-menu">
-                <div className="navbar-end is-size-5 px-5 py-5">
+                  {/*desktop navbar */}
+              <div className="navbar-menu">
+                <div className="navbar-end is-size-5 px-5 py-5 ">
                   <Link href="/" className="navbar-item">home</Link>
                   <Link href="/pages/About" className="navbar-item">about</Link>
                   <Link href="/pages/Profile" className="navbar-item">profile</Link>
                 </div>
               </div>
-
             </nav>
         </div>
 
